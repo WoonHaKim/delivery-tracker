f89c78f775e92a4bd0ac1a054f10906f232d187c
Woona Kim <work@unnakim.info>
2022-04-19 13:16:38 +0900

    Add common package

---
+++ packages/common/carriers/kr.cupost/index.js
@@ -0,0 +1,9 @@
+const cjLogistics = require('../kr.cjlogistics');
+
+module.exports = {
+  info: {
+    name: 'CU 편의점택배',
+    tel: '+8215771287',
+  },
+  getTrack: cjLogistics.getTrack,
+};
---
+++ packages/common/carriers/kr.homepick/index.js
@@ -0,0 +1,9 @@
+const Hanjin = require('../kr.hanjin');
+
+module.exports = {
+  info: {
+    name: '홈픽',
+    tel: '+8218000987',
+  },
+  getTrack: Hanjin.getTrack,
+};
